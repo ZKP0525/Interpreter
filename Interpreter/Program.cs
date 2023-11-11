a7bc64f1ccb2064e477f89b72762aef93ce5ab0f
“JOKP” <zkp0525@163.com>
2023-11-11 12:41:20 +0800

    initialize

---
+++ Interpreter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Interpreter
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
